4d96a08a71a9bb56a3e65a4f0c93b60e471c2b7d
Jeremy Cosel <jmcosel@gmail.com>
2021-02-13 22:24:08 -0800

    Switch out some packages; fix the setup script

--- test/util/setup.js
+++ test/util/setup.js
@@ -1,17 +1,31 @@
-const chai = require('chai');
-chai.use(
-  require('chai-json-schema-ajv').create({
-    allErrors: true
-  })
-);
-global.expect = chai.expect;
-global.should = chai.should;
-
-// All asynchronous code is triggered during module export, so that promises are resolved before any test execution
 module.exports = (async () => {
+  // Set baseUrl
+  global.baseUrl = 'https://petstore.swagger.io/v2';
+
+  // Setup Chai
+  global.chai = require('chai');
+  chai.use(
+    require('chai-json-schema-ajv').create({
+      allErrors: true
+    })
+  );
+  global.expect = chai.expect;
+  global.should = chai.should;
+
+  // Setup Chance
+  global.chance = require('chance').Chance();
+
+  // Setup Fetch
+  global.fetch = require('node-fetch');
+
+  // Setup Swagger Schema
   const SwaggerParser = require('@apidevtools/swagger-parser');
-  // Dynamically load in current Swagger specs for public/private APIs
-  global.schema = await SwaggerParser.dereference(
+  // Dynamically load in current Swagger spec and save it to a variable
+  global.swaggerSpec = await SwaggerParser.dereference(
     `https://petstore.swagger.io/v2/swagger.json`
   );
+  // Function to easily reference response schema from the spec on-demand based on endpoint, verb, and status code
+  global.assignSchema = (endpoint, verb = 'get', statusCode = '200') => {
+    return swaggerSpec.paths[endpoint][verb].responses[statusCode].schema;
+  };
 })();
